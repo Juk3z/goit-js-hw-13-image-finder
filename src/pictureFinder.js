770920ff95e8260b18b5387b7d081b5e13ab6a4e
Maxim Karimov <Juk3z1973@gmail.com>
2021-06-03 23:00:20 +0500

    правки

--- src/pictureFinder.js
+++ src/pictureFinder.js
@@ -9,13 +9,15 @@ let pageCounter = 0;
 
 async function submitHandler(event) {   
     event.preventDefault();
-    let findWord = refs.inputField.value;
+    let findWord = refs.inputField.value.trim();
+    if (!findWord) { return };
     pageCounter += 1;
+
     const responce = await apiService(findWord, pageCounter, 12).then().catch();
     
     listCreating(responce);
     
-    
+    if (responce.length < 12) { return };
     if (pageCounter === 1) {
         buttonCreating();
         refs.buttonElement.addEventListener('click', buttonHandler);
@@ -25,7 +27,6 @@ async function submitHandler(event) {
 
 function buttonHandler() {
     pageCounter += 1;
-    console.log(pageCounter, 'внешний пейдж');
     uploadMore(pageCounter);
 }
 
